73ab833a872a98269271f2a08389dd2061d143ad
Fariha-simi <farihatasnimsimi.physics@gmail.com>
2020-09-24 07:48:27 -0700

    added first file

---
+++ test11.py
@@ -0,0 +1,45 @@
+#Assignment-2:
+a=90
+b=100
+c=80
+d=40
+e=0.45
+d=8.9
+f=0.04546
+
+if a>b>c>d or d>e>f>a>=b:
+    print("First Commit")
+if a>=b and d>=e or a>b:
+    print("Second Commit")
+if a==b or a!=b:
+    print("Third Commit")
+elif d>e>f:
+    print("Forth Commit")
+if c==d and d==f:
+    print("Fifth Commit")
+elif c%d==0:
+    print("Sixth Commmit")
+elif a>=c or e>=d and b<=e and d<a and a==f:
+    print("Seventh Commit")
+else:
+    print("First Else")
+if f!=c:
+    print("Eight Commit")
+if a>b==d:
+    print("Nine Commit")
+elif d==a or e<d:
+    print("Ten Commit")
+elif b%c==0:
+    print("Eleven Commit")
+elif d<e<f:
+    print("Twelve Commit")
+elif f!=e and a>b:
+    print("Thirten Commit")
+else:
+    print("Second Else")
+if c>=a and c>=e>f:
+    print("Forteen Commit")
+else:
+    print("Good Try")
+
+print("The End")
